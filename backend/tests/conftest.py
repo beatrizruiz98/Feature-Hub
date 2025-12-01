58218eafb401129f0a1f61c38a807a822d7ef904
Beatriz Ruiz <beatrizruiz981@gmail.com>
2025-12-01 21:31:41 +0000

    Stop tracking venv

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -6,11 +6,12 @@ from fastapi.testclient import TestClient
 import pytest
 from pytest import fixture
 
-# Cadena de conexión dinámica tomada del archivo .env.
+# Cadena de conexión dinámica tomada del archivo .env con _test para la base de datos de pruebas.
 SQLMODEL_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}/{settings.db_name}_test"
 # Motor de base de datos global para reutilizar conexiones en toda la aplicación.
 engine = create_engine(SQLMODEL_DATABASE_URL, echo=False)
 
+# Fixtures de pytest para configurar la base de datos de pruebas
 @fixture(scope="session")
 def session():
     SQLModel.metadata.drop_all(engine)    
@@ -19,11 +20,14 @@ def session():
     with Session(engine) as test_session:
         yield test_session
         
+# Fixture para el cliente de pruebas FastAPI que llama a session para la base de datos       
 @pytest.fixture(scope="session")
 def client(session):
     """Cliente de pruebas para simular peticiones HTTP a la aplicación FastAPI."""
+    # Sobrescribe la dependencia de get_session para usar la sesión de pruebas
     def override_get_session():
         yield session
+    # Funcion de sobrescritura de la dependencia 
     app.dependency_overrides[get_session] = override_get_session
     with TestClient(app) as test_client:
         yield test_client
